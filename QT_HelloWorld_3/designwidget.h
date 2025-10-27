8f944eff4e0b7cceb7d814d64151a503ac6fa62f
41343204jieyu <41343204@nfu.edu.tw>
2025-10-27 13:38:49 +0800

    dialog

---
+++ QT_HelloWorld_3/designwidget.h
@@ -0,0 +1,25 @@
+#ifndef DESIGNWIDGET_H
+#define DESIGNWIDGET_H
+
+#include <QDialog>
+#include<QtWidgets>
+
+class DesignWidget : public QDialog
+{
+    Q_OBJECT
+
+public:
+    DesignWidget(QWidget *parent = nullptr);
+    ~DesignWidget();
+private:
+    QTextEdit *displayTextEdit;
+    QPushButton *colorPushBtn;
+    QPushButton *errorPushBtn;
+    QPushButton *filePushBtn;
+    QPushButton *fontPushBtn;
+    QPushButton *inputPushBtn;
+    QPushButton *pagePushBtn;
+    QPushButton *progressPushBtn;
+    QPushButton *printPushBtn;
+};
+#endif // DESIGNWIDGET_H
